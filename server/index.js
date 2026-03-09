992b98875ba99964b2473108ea0a771c38e3ed51
gauravgkb99 <gauravkingbudhlakoti9999@gmail.com>
2026-03-09 14:28:22 +0530

    fix: force IPv4 for Gmail SMTP on Render

--- server/index.js
+++ server/index.js
@@ -28,13 +28,16 @@ console.log('📧 Email config:', {
 })
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
     connectionTimeout: 10000,
     socketTimeout: 10000,
+    family: 4,
 })
 
 // Verify transporter on startup
